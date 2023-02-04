20ef78cd1a8f3ebba50bd8d62369423ad791c99f
Okan7171 <122784941+Okan7171@users.noreply.github.com>
2023-02-04 18:53:39 +0300

    conflictsiz bir hayat

--- src/main/java/org/example/newFeature.java
+++ src/main/java/org/example/newFeature.java
@@ -4,5 +4,6 @@ public class newFeature {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Düzeltmeler yapildi");
+        System.out.println("conflic çıksın istemiyoreum");
     }
 }
